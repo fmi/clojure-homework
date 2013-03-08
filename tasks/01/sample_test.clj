c42112963633a0047e88ef76fb736e524954bbb2
Stefan Kanev <stefan.kanev@gmail.com>
2013-03-08 20:12:04 +0200

    Rename bissect to bisect

--- tasks/01/sample_test.clj
+++ tasks/01/sample_test.clj
@@ -7,8 +7,8 @@
     (let [difference 0.001
           fuzzy-equals? #(< (abs (- %1 %2)) difference)
           f #(+ 1 (+ % %))]
-      (is (fuzzy-equals? (- 1/2) (bissect f -100 100 fuzzy-equals?)))
-      (is (fuzzy-equals? (- 1/2) ((make-bissector difference) f -100 100)))))
+      (is (fuzzy-equals? (- 1/2) (bisect f -100 100 fuzzy-equals?)))
+      (is (fuzzy-equals? (- 1/2) ((make-bisector difference) f -100 100)))))
   (testing "queue"
     (is (= "baba"
            (peek-at-queue
