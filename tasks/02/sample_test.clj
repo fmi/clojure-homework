04360151dc45a1c0594ebf11a5fd6403d29b67cb
Nikolay Bachiyski <nb@nikolay.bg>
2013-03-17 20:59:54 +0200

    Add the last two tests

--- tasks/02/sample_test.clj
+++ tasks/02/sample_test.clj
@@ -16,6 +16,17 @@
   (testing "commutative fibonacci"
     (let [p0 {:f0 #(if (= [%1 %2] [0 0]) 0 111)}
           p1 {}]
-      (is (= [1 0] (game p0 p1))))))
+      (is (= [1 0] (game p0 p1)))))
+  (testing "ufffff me now"
+    (let [p0 {:m0 {:vector 1 :map 2 :set 15 :cons 234 :list 7513 :LAZYSEQ 88888 :uumf 99999}
+              :m1 {:cons 231}
+              :m2 {:cons 2}
+              :f (fn [x y] (if (= x 88888) x 5))}
+          p1 {}]
+      (is (= [3 0] (game p0 p1)))))
+  (testing "lazy couples"
+    (def map-with-infinite-lazy {:l0 (range) :l1 (lazy-seq (cons 1 nil)) :s #{0 1 2 3 4 5} :k 11})
+    (let [p0 {:l0 (lazy-seq (cons 1 (lazy-seq [2]))) :l1 (lazy-seq (cons 1 (lazy-seq [6]))) :s0 (set (range 15))}]
+      (is (= [5 0] (game p0 map-with-infinite-lazy))))))
 
 (run-tests)
