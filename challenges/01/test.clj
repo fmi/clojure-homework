ce500907b4d6d33f932c8f3c61fb0ca7f6140042
Hristo Vladev <hr.vladev@gmail.com>
2014-03-05 01:43:54 +0200

    Add solution and full tests for challenge 01

---
+++ challenges/01/test.clj
@@ -0,0 +1,6 @@
+(deftest challenge-01-test
+  (testing "from-digits"
+    (is (= (from-digits []) 0)) 
+    (is (= (from-digits [0]) 0)) 
+    (is (= (from-digits [4 2]) 42)) 
+    (is (= (from-digits [1 0 3 0]) 1030))))
