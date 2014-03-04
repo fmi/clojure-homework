4ab8c754e28b1f75088a9396f4bf4eb1a7fa60be
Stefan Kanev <stefan.kanev@gmail.com>
2014-03-04 20:15:15 +0200

    task 01

---
+++ tasks/01/template.clj
@@ -0,0 +1,20 @@
+(defn digits [n]
+  nil)
+
+(defn prime-factors [n]
+  nil)
+
+(defn fizzbuzz [n]
+  nil)
+
+(defn densities [xs]
+  nil)
+
+(defn index-by [f xs]
+  nil)
+
+(defn harmonic [n]
+  nil)
+
+(defn uniquify [in]
+  nil)
