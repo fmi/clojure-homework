9b6f00e962e36bed73a19618f87ca47ef81356fc
dakatapetrov <daka7a@gmail.com>
2014-03-22 19:43:58 +0200

    Fix the formatting of the sample test of challenge 03.

--- challenges/03/sample_test.clj
+++ challenges/03/sample_test.clj
@@ -10,6 +10,6 @@
     (is (instance? lazy-seq-class (chase 2 coll)))
     (is (instance? lazy-seq-class (chase 8 coll)))
     (is (= (take 4 (chase 2 coll)) expected-1))
-    (is (= (take 7(chase 8 coll)) expected-2))))
+    (is (= (take 7 (chase 8 coll)) expected-2))))
 
 (run-tests)
