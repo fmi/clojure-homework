44c17ce2cbc3c18e01bf0c088c97f91dac8d033b
ivo- <ivailo.nikolaev.hristov@gmail.com>
2014-04-13 23:08:50 +0300

    challenge 05: improvements

--- challenges/05/sample_test.clj
+++ challenges/05/sample_test.clj
@@ -3,7 +3,7 @@
 (load-file "solution.clj")
 
 (deftest challenge-05-sample-test
-  (let [coll (lazyfy
+  (let [coll (lazy
                (+ 1 2)
                (/ 1 0))]
     (is (not (realized? coll)))
